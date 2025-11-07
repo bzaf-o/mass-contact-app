8ff04117269ef2576f320225248ffeb606a3b9fe
bzaf-o <balqiszafirah.o@gmail.com>
2025-11-08 00:23:05 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# saved_messages.py
+saved_messages = {
+    "BTAR Promo": """Do mark your calendar for BTAR scholarship applications, it usually opens early February. 
+
+It's a scholarship under YTAR, full tuition coverage for all MQA-accredited courses and competitive allowance. No bond to serve, but must complete a community project before graduation.
+
+You can read more here: https://www.yayasantar.org.my/tarscholarship""",
+    
+    "Nudge": """Hello, just bumping this message a bit. I would really appreciate your response on this whenever it's possible for you :) 
+
+In case you prefer not to be contacted further/are not XX, then please let me know as well. Thank you!"""
+}
